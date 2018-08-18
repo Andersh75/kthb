0214e9bb8b20db265ee6f0e66afd33646ef2aeee
Andersh75 <andersh75@gmail.com>
2018-08-18 19:00:36 +0200

    Done

--- brokerworker.js
+++ brokerworker.js
@@ -1,22 +1,24 @@
-importScripts('node_modules/pouchdb/dist/pouchdb.min.js');
-importScripts('node_modules/pouchdb/dist/pouchdb.find.min.js');
+/*  eslint-env worker */
+importScripts('node_modules/pouchdb/dist/pouchdb.min.js')
+importScripts('node_modules/pouchdb/dist/pouchdb.find.min.js')
 
-var db = new PouchDB('http://localhost:5984/statistics');
+/*  global PouchDB  */
+var db = new PouchDB('http://localhost:5984/statistics')
 
-onmessage = async function(e) {
-  //console.log('Message received from main script');
+onmessage = async e => {
+  //  console.log('Message received from main script');
   const res = await db.query('my_index8/by_name', {
     startkey: 'A',
     reduce: true,
     group: true,
     limit: e.data.limit,
     skip: e.data.skip
-  });
+  })
 
-  let concatData = e.data.tableitems.concat(res.rows);
-  e.data.tableitems = concatData;
+  let concatData = e.data.tableitems.concat(res.rows)
+  e.data.tableitems = concatData
 
-  var workerResult = {'rows': res.rows, 'tableitems': e.data.tableitems};
-  //console.log('Posting message back to main script');
-  postMessage(workerResult);
+  var workerResult = { rows: res.rows, tableitems: e.data.tableitems }
+  //  console.log('Posting message back to main script');
+  postMessage(workerResult)
 }
